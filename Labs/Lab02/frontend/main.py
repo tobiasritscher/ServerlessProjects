74c68eaf1f58f6fcec403bea5d53383274dabf9c
Adrian Hornung <adrian@nzxt-arch.localdomain>
2021-10-08 18:40:54 +0200

    finished with initial code base

--- Labs/Lab02/frontend/main.py
+++ Labs/Lab02/frontend/main.py
@@ -1,6 +1,10 @@
 import os
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 from flask import abort, render_template
 import logging
+from wtforms.csrf.session import SessionCSRF
+from datetime import timedelta
 
 DEBUG = True
 
@@ -8,29 +12,44 @@ class Config:
     DATABASE_PWD  = os.environ.get("DATABASE_PWD", default=None)
     DATABASE_USER = os.environ.get("DATABASE_USER", default=None)
     DATABASE_NAME = os.environ.get("DATABASE_NAME", default=None)
+    # TODO: remove deault and replace with None
+    CSRF_SECRET = os.environ.get("CSRF_SECRET", default="EPj00jpfj8Gx1SjnyLxwBBSQfnQ9DJYe0Ym").encode("utf8")
 
     @classmethod
-    def is_setup(cls):
-        values = [
-                cls.DATABASE_PWD,  
-                cls.DATABASE_USER, 
-                cls.DATABASE_NAME 
-        ]
-        for val in values:
-            if val is None:
-                return False
-        return True
+    def not_setup(cls):
+        values = []
+
+        for val in dir(cls):
+            if callable(getattr(cls, val)) or val.startswith("_"):
+                continue
+            if getattr(cls, val) is None:
+                values.append(val)
+        return values
+
+class FrontendForm(FlaskForm):
+    text = StringField('Text')
+    submit = SubmitField('Submit')
+
+    class Meta:
+        csrf = False
+        csrf_class = SessionCSRF
+        csrf_secret = Config.CSRF_SECRET 
+        csrf_time_limit = timedelta(minutes=20)
+
 
 
 def main(request):
-    # make the IDE shutup
-    _ = request
     # check if function is setup correctly
-    if not Config.is_setup():
-        logging.error("missing database configuraion")
+    if len(conf := Config.not_setup()) > 0:
+        logging.error(f"missing database configuraion for {conf}")
         if not DEBUG:
             abort(500)
 
-    user = {'username': "Adrian"}
-
-    return render_template("index.html", title="Form", user=user)
+    form = FrontendForm()
+    if request.method == "GET":
+        return render_template("form.html", title="Form", form=form)
+    elif request.method == "POST":
+        # TODO: handle parsing of the form
+        return render_template("form_submitted.html", title="Sucess submitting", form=form)
+    else:
+        abort(405)
