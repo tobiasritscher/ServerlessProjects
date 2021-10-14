74cfe53ed8b0548024d4669ca4e54443f28a4746
Sydney Nguyen <nguyesyd@students.zhaw.ch>
2021-10-14 17:16:39 +0200

    Create Dummy04.py

---
+++ Labs/Lab03/Dummyfunctions/Dummy04.py
@@ -0,0 +1,12 @@
+def hello_world(request): 
+    if (request != {}):
+        a = 1
+        b = 100
+        c = 123
+        d = a*b
+        d = d*c
+        return "Function 4 has used some multiplication."
+    return ""
+
+#print(hello_world({"hihihihi":"hi"}))
+#print(hello_world({}))
