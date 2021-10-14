19450e713d163f2b23104a8ea6cb299c66de1ffb
Sydney Nguyen <nguyesyd@students.zhaw.ch>
2021-10-14 17:18:23 +0200

    Update Dummy03.py

--- Labs/Lab03/Dummyfunctions/Dummy03.py
+++ Labs/Lab03/Dummyfunctions/Dummy03.py
@@ -2,7 +2,7 @@ def hello_world(request):
     if (request != {}):
         time.sleep(2)
         return "Function 3 has returned nicely after some sleep."
-    return ""
+    return {}
 
 #print(hello_world({"hihihihi":"hi"}))
 #print(hello_world({}))
