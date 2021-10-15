90211c9d6401a3d567d0d9d1f445a43116d04442
kuendkun <kuendkun@students.zhaw.ch>
2021-10-15 12:09:45 +0200

    correct dummy04

--- Labs/Lab03/Dummyfunctions/Dummy03.py
+++ Labs/Lab03/Dummyfunctions/Dummy03.py
@@ -1,10 +1,11 @@
 import time
 
-def hello_world(request): 
-    if (request != {}):
-        time.sleep(0.2)
-        return {"text":"Function 3 has returned nicely after some sleep."}
-    return {}
+def hello_world(request):
+  request_json = request.get_json()
+  if request_json:
+    time.sleep(0.2)
+    return {"text":"Function 3 has returned nicely after some sleep."}
+  return {}
 
 #print(hello_world({"hihihihi":"hi"}))
 #print(hello_world({}))
