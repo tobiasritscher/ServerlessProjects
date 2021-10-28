6f89312913453268d20d46f2c1831d7f8399eda8
hornuadr <hornuadr@students.zhaw.ch>
2021-10-28 15:33:41 +0200

    done

---
+++ Labs/Lab04/trigger/main.py
@@ -0,0 +1,90 @@
+import os
+import json
+import uuid
+import logging
+import datetime 
+
+
+import flask 
+import flask_wtf 
+import wtforms 
+from wtforms.csrf.session import SessionCSRF
+from google.cloud import storage
+
+class Config:
+    BUCKET_NAME         = "gcf-sources-454851464893-europe-west6-data"
+    CSRF_SECRET         = os.environ.get("CSRF_SECRET", default="")
+    
+    @classmethod
+    def not_setup(cls):
+        values = []
+
+        for val in dir(cls):
+            inner = getattr(cls, val) 
+            if callable(inner) or val.startswith("_"):
+                continue
+            if inner is None or inner == "":
+                values.append(val)
+        return values
+
+
+class FrontendForm(flask_wtf.FlaskForm):
+    text = wtforms.TextAreaField('Text', validators=[wtforms.validators.DataRequired()])
+    submit = wtforms.SubmitField('Submit')
+
+    class Meta:
+        csrf = False
+        csrf_class = SessionCSRF
+        csrf_secret = Config.CSRF_SECRET 
+        csrf_time_limit = datetime.timedelta(minutes=20)
+
+class Results:
+    def __init__(self, text):
+        self.text = text
+        self.datetime = datetime.datetime.now(datetime.timezone.utc).isoformat()
+
+    def to_obj(self):
+        return {
+                "feedback_text": self.text,
+                "timestamp": self.datetime
+        }
+
+def handle_invalid_form(request, form):
+    return handle_get(request, form)
+
+def handle_post(request, form):
+    if form.validate_on_submit():
+        res = Results(form.text.data)
+
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(Config.BUCKET_NAME)
+
+        # create unique name for data
+        uid = uuid.uuid4() 
+        filename = f"user-information-{uid.hex}.txt"
+        blob = bucket.blob(filename)
+        blob.upload_from_string(json.dumps(res.to_obj()))
+
+        return flask.render_template("form_submitted.html", title="Sucess submitting", result=res, filename=filename)
+    else:
+        return handle_invalid_form(request,form)
+
+def handle_get(request, form):
+    # make the linter shutup
+    _ = request
+    return flask.render_template("form.html", title="Form", form=form)
+
+def main(request):
+    # check if function is setup correctly
+    if len(conf := Config.not_setup()) > 0:
+        logging.error(f"missing database configuraion for {conf}")
+        flask.abort(500)
+
+    form = FrontendForm()
+    if request.method == "GET":
+        return handle_get(request, form)
+    elif request.method == "POST":
+        return handle_post(request, form)
+    else:
+        flask.abort(405)
+
