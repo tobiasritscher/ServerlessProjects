63789603e9feaaed35509952c472f003681316e0
rentsjar <rentsjar@students.zhaw.ch>
2021-11-25 17:31:25 +0100

    Update app.py

--- Labs/Lab07/display_blogs/app.py
+++ Labs/Lab07/display_blogs/app.py
@@ -3,18 +3,20 @@ import requests
 import flask
 import datetime
 app = flask.Flask(__name__)
+#baseAdress = 'https://scaddb.ritscher.ch'
+baseAdress = os.environ.get("DB_IP")
 
 @app.route('/', methods=['GET','POST'])
 def main():  
     
     if flask.request.method == "POST":
-        r = requests.post('https://scaddb.ritscher.ch/set', json={
+        r = requests.post('https://'+baseAdress+'/set', json={
             "text": flask.request.form["send_blog"],
             "timestamp" : datetime.datetime.now(datetime.timezone.utc ).isoformat()
         })
         r.raise_for_status()
 
-    r = requests.get('https://scaddb.ritscher.ch/get')
+    r = requests.get('https://'+baseAdress+'/get')
     r.raise_for_status()
     myJson = r.json()["all"]
     return flask.render_template("index.html", title="Our Special Blog", blogs=myJson)
