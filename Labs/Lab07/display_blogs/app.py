0fb403fbeb7c8c578b18925552dac35e1e5fb0aa
rentsjar <rentsjar@students.zhaw.ch>
2021-11-25 16:45:16 +0100

    Add files via upload

---
+++ Labs/Lab07/display_blogs/app.py
@@ -0,0 +1,24 @@
+import os
+import requests
+import flask
+import datetime
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def main():  
+    
+    if flask.request.method == "POST":
+        r = requests.post('https://scaddb.ritscher.ch/set', json={
+            "text": flask.request.form["send_blog"],
+            "timestamp" : datetime.datetime.now(datetime.timezone.utc ).isoformat()
+        })
+        r.raise_for_status()
+
+    r = requests.get('https://scaddb.ritscher.ch/get')
+    r.raise_for_status()
+    myJson = r.json()["all"]
+    return flask.render_template("index.html", title="Our Special Blog", blogs=myJson)
+
+    
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0',port=int(os.environ.get('Port', 8000))) 
