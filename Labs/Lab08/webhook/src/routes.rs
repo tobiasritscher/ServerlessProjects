4d6579d1ce8e6242a9dabf7d4ad61e9748b604ed
Adrian Hornung <hornuadr@students.zhaw.ch>
2021-12-28 20:06:29 +0100

    add root route

--- Labs/Lab08/webhook/src/routes.rs
+++ Labs/Lab08/webhook/src/routes.rs
@@ -1,16 +1,49 @@
 use actix_web::dev::RequestHead;
 use actix_web::http::header::ContentType;
 use actix_web::http::StatusCode;
-use actix_web::{get, post, web, Either, HttpResponse, HttpResponseBuilder, Responder};
+use actix_web::{
+    get, post, web, Either, HttpRequest, HttpResponse, HttpResponseBuilder, Responder, Result,
+};
 
 use crate::{model::Info, storage};
 
 pub fn get_scope(path: &str) -> actix_web::Scope {
-    web::scope(path).service(webhook).service(stats)
+    web::scope(path)
+        .service(root)
+        .service(webhook)
+        .service(stats)
+}
+
+#[get("/")]
+async fn root(req: HttpRequest) -> Result<impl Responder> {
+    let url_webhook = req.url_for("webhook", &["/"])?;
+    let url_stats = req.url_for("stats", &["/"])?;
+
+    let res = format!(
+        r#"
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SCAD-CLUELESS Webhook</title>
+</head>
+<body>
+  <!-- your content here... -->
+  <p> Please use the <a href="{}">Webhook</a> or  <a href="{}">Stats</a></p>
+</body>
+</html>
+"#,
+        url_webhook, url_stats
+    );
+
+    Ok(HttpResponseBuilder::new(StatusCode::OK)
+        .insert_header(ContentType::html())
+        .body(res))
 }
 
 /// extract `Info` using serde
-#[post("/webhook", guard = "webhook_guard")]
+#[post("/webhook", name = "webhook", guard = "webhook_guard")]
 async fn webhook(
     storage: web::Data<storage::Storage<Info>>,
     info: Either<web::Json<Info>, String>,
@@ -56,7 +89,7 @@ fn webhook_guard(req: &RequestHead) -> bool {
     }
 }
 
-#[get("/stats")]
+#[get("/stats", name = "stats")]
 async fn stats(storage: web::Data<storage::Storage<Info>>) -> impl Responder {
     log::debug!("webhook stats");
 
